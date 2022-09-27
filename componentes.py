658adfb47cab476da01a0bcdae0c6d76a301bf03
JoaoLucasMBC <joaolucasmbcadorniga@gmail.com>
2022-09-27 13:41:52 -0300

    exe4

--- componentes.py
+++ componentes.py
@@ -37,8 +37,36 @@ def exe3(x1, x0, y1, y0, z3, z2, z1, z0):
 
 
 def exe4_ula(a, b, inverte_a, inverte_b, c_in, c_out, selecao, zero, resultado):
+    negA = Signal(modbv(0)[32:])
+    negB = Signal(modbv(0)[32:])
+    
+    muxA_out = Signal(modbv(0)[32:])
+    muxB_out = Signal(modbv(0)[32:])
+    mux4_out = Signal(modbv(0)[32:])
+
+    add_out = Signal(modbv(0)[32:])
+    and_out = Signal(modbv(0)[32:])
+    or_out = Signal(modbv(0)[32:])
+
+    zerado = Signal(modbv(0)[32:])
+
+    muxA = mux2way(muxA_out, a, negA, inverte_a)
+
+    muxB = mux2way(muxB_out, b, negB, inverte_b)
+
+    addAB = adder(add_out, c_out, muxA_out, muxB_out, c_in)
+
+    mux4 = mux4way(mux4_out, and_out, or_out, add_out, zerado, selecao)
+    
     @always_comb
     def comb():
-        resultado.next = 0
+        negA.next = not a
+        negB.next = not b
+
+        and_out.next = muxA_out and muxB_out
+        or_out.next = muxA_out or muxB_out
+
+        resultado.next = mux4_out
+        zero.next = not(mux4_out or mux4_out)
 
     return instances()
